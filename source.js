2c9a7ce43e71e3943f5c2b562538cbb3dd0ab34e
Riki S <53028102+rikiginqo@users.noreply.github.com>
2022-08-15 17:39:59 +0700

    Update source.js

--- source.js
+++ source.js
@@ -6,7 +6,7 @@ if (field_name == null || field_name == "") {
     (async() => {
 
             try {
-                let app = await require('qlik').currApp();
+                let app = await require('qlik').currApp(); // open connection to the current qliksense app
                 let field = await app.model.enigmaModel.getField({"qFieldName": field_name}); // check if field exists
                 let scramble_result = await app.model.enigmaModel.scramble({"qFieldName": field_name}); // execute Qlik Scramble API
                 let save_result = await app.doSave({"qFileName": field_name}) // save scrambled value
